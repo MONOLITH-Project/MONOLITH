6825b1b16138a6e5fc6fa20846c81eaffae6c7fb
Mr.UNIX <mrunix00@protonmail.com>
2025-04-11 21:38:10 +0100

    Fix memory mmap search

--- kernel/memory/pmm.c
+++ kernel/memory/pmm.c
@@ -18,7 +18,7 @@ extern size_t stack_bottom;
 
 struct multiboot_tag_mmap *find_mmap_tag(struct multiboot_tag *tag)
 {
-    /* The first 8 bytes of the multiboot info are the total size and reserved field */
+    /* Skip the total size and reserved field */
     struct multiboot_tag *current_tag = (struct multiboot_tag *) ((uint8_t *) tag + 8);
 
     while (current_tag->type != MULTIBOOT_TAG_TYPE_END) {
