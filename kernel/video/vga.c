8f5a98cc32bfef4957e4ce8d381fe638e1ab7657
Mr.UNIX <mrunix00@protonmail.com>
2025-04-18 22:18:33 +0100

    Making the naming more consistent in vga.c

--- kernel/video/vga.c
+++ kernel/video/vga.c
@@ -10,20 +10,20 @@
 #define VIDEO_WIDTH 80
 #define VIDEO_HEIGHT 25
 
-static int cursor_x = 0;
-static int cursor_y = 0;
-static uint8_t vga_attr = 0x07;
+static int _cursor_x = 0;
+static int _cursor_y = 0;
+static uint8_t _vga_attr = 0x07;
 
-void update_cursor()
+static void _update_cursor()
 {
-    unsigned short position = cursor_y * VIDEO_WIDTH + cursor_x;
+    unsigned short position = _cursor_y * VIDEO_WIDTH + _cursor_x;
     outb(0x0F, 0x3D4);
     outb((unsigned char) (position & 0xFF), 0x3D5);
     outb(0x0E, 0x3D4);
     outb((unsigned char) ((position >> 8) & 0xFF), 0x3D5);
 }
 
-void scroll()
+static void _scroll()
 {
     /* Move everything up by one line */
     for (int y = 0; y < VIDEO_HEIGHT - 1; y++) {
@@ -40,7 +40,7 @@ void scroll()
     for (int x = 0; x < VIDEO_WIDTH; x++) {
         int idx = 2 * ((VIDEO_HEIGHT - 1) * VIDEO_WIDTH + x);
         VIDEO_MEMORY[idx] = ' ';
-        VIDEO_MEMORY[idx + 1] = vga_attr;
+        VIDEO_MEMORY[idx + 1] = _vga_attr;
     }
 }
 
@@ -48,49 +48,49 @@ void vga_putchar(char c)
 {
     switch (c) {
     case '\n':
-        cursor_x = 0;
-        cursor_y++;
-        if (cursor_y >= VIDEO_HEIGHT) {
-            scroll();
-            cursor_y = VIDEO_HEIGHT - 1;
+        _cursor_x = 0;
+        _cursor_y++;
+        if (_cursor_y >= VIDEO_HEIGHT) {
+            _scroll();
+            _cursor_y = VIDEO_HEIGHT - 1;
         }
         break;
     case '\b':
-        if (cursor_x > 0) {
-            cursor_x--;
-            VIDEO_MEMORY[2 * (cursor_y * VIDEO_WIDTH + cursor_x)] = ' ';
-            VIDEO_MEMORY[2 * (cursor_y * VIDEO_WIDTH + cursor_x) + 1] = vga_attr;
-        } else if (cursor_y > 0) {
-            cursor_y--;
-            cursor_x = VIDEO_WIDTH - 1;
-            VIDEO_MEMORY[2 * (cursor_y * VIDEO_WIDTH + cursor_x)] = ' ';
-            VIDEO_MEMORY[2 * (cursor_y * VIDEO_WIDTH + cursor_x) + 1] = vga_attr;
+        if (_cursor_x > 0) {
+            _cursor_x--;
+            VIDEO_MEMORY[2 * (_cursor_y * VIDEO_WIDTH + _cursor_x)] = ' ';
+            VIDEO_MEMORY[2 * (_cursor_y * VIDEO_WIDTH + _cursor_x) + 1] = _vga_attr;
+        } else if (_cursor_y > 0) {
+            _cursor_y--;
+            _cursor_x = VIDEO_WIDTH - 1;
+            VIDEO_MEMORY[2 * (_cursor_y * VIDEO_WIDTH + _cursor_x)] = ' ';
+            VIDEO_MEMORY[2 * (_cursor_y * VIDEO_WIDTH + _cursor_x) + 1] = _vga_attr;
         }
         break;
     case '\r':
-        cursor_x = 0;
+        _cursor_x = 0;
         break;
     case '\t':
         do {
-            VIDEO_MEMORY[2 * (cursor_y * VIDEO_WIDTH + cursor_x)] = ' ';
-            VIDEO_MEMORY[2 * (cursor_y * VIDEO_WIDTH + cursor_x) + 1] = vga_attr;
-            cursor_x++;
-        } while (cursor_x % 8 != 0);
+            VIDEO_MEMORY[2 * (_cursor_y * VIDEO_WIDTH + _cursor_x)] = ' ';
+            VIDEO_MEMORY[2 * (_cursor_y * VIDEO_WIDTH + _cursor_x) + 1] = _vga_attr;
+            _cursor_x++;
+        } while (_cursor_x % 8 != 0);
         break;
     default:
-        VIDEO_MEMORY[2 * (cursor_y * VIDEO_WIDTH + cursor_x)] = c;
-        VIDEO_MEMORY[2 * (cursor_y * VIDEO_WIDTH + cursor_x) + 1] = vga_attr;
-        cursor_x++;
-        if (cursor_x >= VIDEO_WIDTH) {
-            cursor_x = 0;
-            cursor_y++;
-            if (cursor_y >= VIDEO_HEIGHT) {
-                scroll();
-                cursor_y = VIDEO_HEIGHT - 1;
+        VIDEO_MEMORY[2 * (_cursor_y * VIDEO_WIDTH + _cursor_x)] = c;
+        VIDEO_MEMORY[2 * (_cursor_y * VIDEO_WIDTH + _cursor_x) + 1] = _vga_attr;
+        _cursor_x++;
+        if (_cursor_x >= VIDEO_WIDTH) {
+            _cursor_x = 0;
+            _cursor_y++;
+            if (_cursor_y >= VIDEO_HEIGHT) {
+                _scroll();
+                _cursor_y = VIDEO_HEIGHT - 1;
             }
         }
     }
-    update_cursor();
+    _update_cursor();
 }
 
 void vga_puts(const char *str)
@@ -105,29 +105,29 @@ void vga_clear()
         VIDEO_MEMORY[2 * i] = ' ';
         VIDEO_MEMORY[2 * i + 1] = 0x07;
     }
-    cursor_x = 0;
-    cursor_y = 0;
-    update_cursor();
+    _cursor_x = 0;
+    _cursor_y = 0;
+    _update_cursor();
 }
 
 void vga_set_bg_color(vga_color_t color)
 {
-    vga_attr = (color << 4) | 0x07;
+    _vga_attr = (color << 4) | 0x07;
 }
 
 void vga_set_fg_color(vga_color_t color)
 {
-    vga_attr = (color & 0x0F) | (vga_attr & 0xF0);
+    _vga_attr = (color & 0x0F) | (_vga_attr & 0xF0);
 }
 
 void vga_clear_with_color(vga_color_t color)
 {
-    vga_attr = (color << 4) | 0x07;
+    _vga_attr = (color << 4) | 0x07;
     for (int i = 0; i < VIDEO_WIDTH * VIDEO_HEIGHT; i++) {
         VIDEO_MEMORY[2 * i] = ' ';
-        VIDEO_MEMORY[2 * i + 1] = vga_attr;
+        VIDEO_MEMORY[2 * i + 1] = _vga_attr;
     }
-    cursor_x = 0;
-    cursor_y = 0;
-    update_cursor();
+    _cursor_x = 0;
+    _cursor_y = 0;
+    _update_cursor();
 }
